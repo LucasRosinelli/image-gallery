9a7d047ee6aa414f409e70ec20c5b4081feef209
Lucas Rosinelli da Rocha <lucas.rosinelli@gmail.com>
2019-06-29 08:14:19 +0100

    mongoose configured

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('YOUR_MONGODB_CONNECTION_HERE', {
+  useNewUrlParser: true,
+});
 
 app.use(require('./routes'));
 
