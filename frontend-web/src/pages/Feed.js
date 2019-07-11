4dd88d81430d875763dcbf0b29b3922289d96172
Lucas Rosinelli da Rocha <lucas.rosinelli@gmail.com>
2019-07-11 16:02:19 +0100

    Feed loaded from API

--- frontend-web/src/pages/Feed.js
+++ frontend-web/src/pages/Feed.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import api from '../services/api';
 
 import './Feed.css';
 
@@ -6,65 +7,60 @@ import more from '../assets/more.svg';
 import like from '../assets/like.svg';
 
 class Feed extends Component {
+  state = {
+    posts: []
+  };
+
+  async componentDidMount() {
+    const response = await api.get('post');
+
+    this.setState({ posts: response.data });
+  }
+
   render() {
     return (
       <section id="post-list">
-        <article>
-          <header>
-            <div className="user-info">
-              <span className="author">lucasrosinelli</span>
-              <span className="place">Amsterdam, The Netherlands</span>
-            </div>
+        { this.state.posts.map(post => (
+          <article key={post._id}>
+            <header>
+              <div className="user-info">
+                <span className="author">{post.author}</span>
+                <span className="place">{post.place}</span>
+              </div>
 
-            <img src={more} alt="More" />
-          </header>
+              <img src={more} alt="More" />
+            </header>
 
-          <img src="https://ichef.bbci.co.uk/news/320/media/images/83351000/jpg/_83351965_explorer273lincolnshirewoldssouthpicturebynicholassilkstone.jpg" alt="Amsterdam Central" />
+            <img src={`http://localhost:3333/files/${post.imageUrl}`} alt={post.description} />
 
-          <footer>
-            <div className="actions">
-              <img src={like} alt="Like" />
-              <img src={like} alt="Like" />
-            </div>
+            <footer>
+              <div className="actions">
+                <img src={like} alt="Like" />
+              </div>
 
-            <strong>5 likes</strong>
+              <strong>{this.getLikesText(post.likes)}</strong>
 
-            <p>
-              <span className="description">Amsterdam Central</span>
-              <span className="hashtags">#europe #netherlands #amsterdam #amsterdamcentral</span>
-              <time className="timestamp" dateTime="2019-06-30T22:20:29.577Z" title="June 30, 2019">June 30, 2019</time>
-            </p>
-          </footer>
-        </article>
-        <article>
-          <header>
-            <div className="user-info">
-              <span className="author">lucasrosinelli</span>
-              <span className="place">Amsterdam, The Netherlands</span>
-            </div>
-
-            <img src={more} alt="More" />
-          </header>
-
-          <img src="https://ichef.bbci.co.uk/news/320/media/images/83351000/jpg/_83351965_explorer273lincolnshirewoldssouthpicturebynicholassilkstone.jpg" alt="Amsterdam Central" />
-
-          <footer>
-            <div className="actions">
-              <img src={like} alt="Like" />
-              <img src={like} alt="Like" />
-            </div>
-
-            <strong>5 likes</strong>
-
-            <p>
-              <span className="description">Amsterdam Central</span>
-              <span className="hashtags">#europe #netherlands #amsterdam #amsterdamcentral</span>
-              <time className="timestamp" dateTime="2019-06-30T22:20:29.577Z" title="June 30, 2019">June 30, 2019</time>
-            </p>
-          </footer>
-        </article>
+              <p>
+                <span className="description">{post.description}</span>
+                <span className="hashtags">{post.hashtags}</span>
+                <time className="timestamp" dateTime={post.createdAt} title={post.createdAt}>{post.createdAt}</time>
+              </p>
+            </footer>
+          </article>
+        )) }
       </section>
     );
+  }
+
+  getLikesText(likes) {
+    if (likes > 1) {
+      return `${likes} likes`;
+    }
+    if (likes === 1) {
+      return '1 like';
+    }
+
+    return 'No likes yet';
   }
 }
 
